53e13bb8c397f91ba723d6d811ab1084f9ac3e9f
Alan <alanremanan@gmail.com>
2026-05-17 18:32:08 +0530

    updated about

--- src/data/about.ts
+++ src/data/about.ts
@@ -2,5 +2,5 @@ export const about = {
   title: "About Me",
 
   description:
-    "Backend & Generative AI Engineer specializing in scalable APIs, distributed systems, and agentic AI. Experienced in orchestrating advanced LLM workflows (LangGraph, CrewAI) and building real-time, production-grade AI platforms from the ground up.",
+    "Hello there,\n\nI'm a Backend & Generative AI Engineer specializing in scalable APIs, distributed systems, and agentic AI. Experienced in orchestrating advanced LLM workflows using LangGraph and CrewAI, and building real-time, production-grade AI platforms from the ground up.",
 };
